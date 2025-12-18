39a7c2c37c7ce94a6d045c844144fd9cb191824e
WhoRegisteredAllTheGoodUsernames <38054565+WhoRegisteredAllTheGoodUsernames@users.noreply.github.com>
2025-12-18 00:06:17 -0300

    Fix Tailwind not detecting dynamic classes

--- my-app/app/ui/button.tsx
+++ my-app/app/ui/button.tsx
@@ -7,9 +7,17 @@ export default function Button({
 	baseColor: string,
 	action: MouseEventHandler<HTMLButtonElement>
 }) {
+	// Not very neat, but could be expanded for more colors
+	let classes = "rounded-sm p-2 "
+	if (baseColor == "stone") {
+		classes += "active:bg-stone-200 bg-stone-300"
+	} else {
+		classes += "active:bg-sky-200 bg-sky-300"
+	}
+
 	return (
 		<button
-			className={`rounded-sm active:bg-${baseColor}-200 bg-${baseColor}-300 p-2`}
+			className={classes}
 			onClick={action}>{text}</button>
 	)
 }
